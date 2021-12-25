10a8abd920b2dcd81d5d38c9d5b5adeb3f62fe4b
Lucas Thiago Batista <lucas.batista@santander.com.br>
2021-12-25 10:56:37 -0300

    add client structure and todos

---
+++ prime/prime_client/client.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, I'm the Prime Client...")
+	//TODO: implementar requisição que passa o numero a ser decomposto
+	doPrimeDecompositionRequest()
+}
+
+func doPrimeDecompositionRequest() error {
+	//TODO: Implementar
+	fmt.Println("Starting Prime Request RPC...")
+	return nil
+}
